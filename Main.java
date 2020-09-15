e7a6e4a2177868f671ae7aafe4412cb0bca6f0f6
demie4744 <demie4744@wrdsb.ca>
2020-09-15 16:12:50 +0000

    I made greet

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program greets the user and calculates their approximate age
+ * @author Erkam
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+
     
+      // declare a varible that will store a name
+    String name; 
+
+System.out.println("Please enter your name:");
+ // initalize the user's name
+ name = input.nextLine();
+
+  // say hello to the user
+       System.out.println("Hello " + name + "!");
+
+  // ask the user for their Year of birth
+System.out.println("Please enter the year you were born:");
+// declare and initalize variable for the birth year
+int birthYear = input.nextInt();
+
+// declare and initalize constant for the current year
+final int CURRENT_YEAR = 2020;
+
+// declare and calculate the user's age
+int age = CURRENT_YEAR - birthYear;
+
+// tell the user their age
+System.out.println(" You are " + age + " years old.");
   }
 }
